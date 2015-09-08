55df5804f99810d8f0dc59f68793d8f703159808
JimQi <jim.qi101@gmail.com>
2015-09-08 14:44:34 -0700

    added getPosterID(name) function

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+function getPosterNames() {
 	var xPathResult = document.evaluate(
 		"//div[@id='ips_Posts']//span[@itemprop='name']",
 		document,
@@ -8,9 +8,27 @@ window.onload = function() {
 
 	var poster_names = [];
 	var xPathNode = xPathResult.iterateNext();
-	while(temp) {
+	while(xPathNode) {
 		poster_names.push(xPathNode.textContent);
-		temp =  xPathResult.iterateNext();
+		xPathNode =  xPathResult.iterateNext();
 	}
 	alert(poster_names[0]);
+	return poster_names;
+}
+
+function getPosterID(posterName) {
+	var xPathResult = document.evaluate(
+		"//div[@id='ips_Posts']//a[@hovercard-id='" + posterName + "']",
+		document,
+		null,
+		XPathResult.FIRST_ORDERED_NODE_TYPE,
+		null);
+
+	var xPathNode = xPathResult.singleNodeValue;
+	alert(xPathNode);
+}
+
+window.onload = function() {
+	var names = getPosterNames();
+	getPosterID(names[0]);
 };
